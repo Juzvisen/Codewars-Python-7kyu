757fb8019cc1423eb763a71a88a0dcd5646e27cf
Juzvisen <juzvisen@hotmail.com>
2021-08-18 18:20:01 +0200

    solution

---
+++ alternate_logic.py
@@ -0,0 +1,5 @@
+# https://www.codewars.com/kata/58f625e20290fb29c3000056/python
+
+
+def alt_or(lst):
+	return any(lst) if lst else None
---
+++ ce*s*r*d_strings.py
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/5ff6060ed14f4100106d8e6f/python
+
+
+def uncensor(infected, discovered):
+	return infected.replace("*", "{}").format(*discovered)
+
+
