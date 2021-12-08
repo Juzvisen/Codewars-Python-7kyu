a7dd28defcb5f124b4a8a11a93f995d528176541
Juzvisen <juzvisen@hotmail.com>
2021-12-08 18:38:45 +0100

    solution

---
+++ finding_remainder_without_using_%_operator.py
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/564f458b4d75e24fc9000041
+
+
+def remainder(a,b):
+	x = int(a / b)
+	return a - x * b
+
--- regex_validate_pin_code.py
+++ regex_validate_pin_code.py
@@ -4,4 +4,3 @@
 def validate_pin(pin):
 	return len(pin) in (4,6) and pin.isdigit()
 
-print(validate_pin("0000"))
